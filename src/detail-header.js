50f746faa000771978e813fc7b469871a064cbad
gaotong <gaotong003@ke.com>
2019-07-20 22:46:00 +0800

    详情页面

---
+++ src/detail-header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./detail.css";
+
+function DetailHeader() {
+  return (
+    <div className="ant-layout">
+      <div className="cct-header">人店平台黑客穆拉松</div>
+      <div className="menu-item-wrapper">人店平台黑客穆拉松</div>
+      <div className="ant-layout-content">人店平台黑客穆拉松</div>
+      <div className="ant-layout-content">人店平台黑客穆拉松</div>
+
+      <p>人店平台黑客穆拉松</p>
+    </div>
+  );
+}
+
+export default DetailHeader;
